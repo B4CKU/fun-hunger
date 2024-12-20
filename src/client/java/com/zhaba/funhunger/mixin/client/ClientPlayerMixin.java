0a77ce95aabff6c5a6f11db604bd9b2f3e2ec367
B4CKU <szymekstrojny@gmail.com>
2024-12-20 11:03:45 +0100

    renamed and documented a mixin

--- src/client/java/com/zhaba/funhunger/mixin/client/ClientPlayerMixin.java
+++ src/client/java/com/zhaba/funhunger/mixin/client/ClientPlayerMixin.java
@@ -8,11 +8,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
-public class SprintClientMixin {
+public class ClientPlayerMixin {
 
+    //This mixin changes the hunger requirement of the sprint into a health requirement.
+    //Otherwise it's functionally pretty much the same as vanilla
     @Inject(method = "canSprint", at = @At("TAIL"), cancellable = true)
     private void sprintingOverride(CallbackInfoReturnable<Boolean> cir) {
-        PlayerEntity player = (PlayerEntity)(Object) this;
+        PlayerEntity player = (PlayerEntity)(Object) this; //a smart way of referencing "this" in the context of a mixin
         cir.setReturnValue(player.hasVehicle() || player.getHealth() > 6.0F || player.getAbilities().allowFlying);
     }
 
